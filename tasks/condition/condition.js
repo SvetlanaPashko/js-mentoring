42ba41ea62bbcf75a3ce2d0d49795255cbdeb5b4
Sviatlana Pashko <s11.pashko@gmail.com>
2020-06-23 21:19:43 +0300

    commit3

--- tasks/condition/condition.js
+++ tasks/condition/condition.js
@@ -4,8 +4,8 @@
  * Task - rewrite IF..ELSE to ternary operator
  */
 {
-  const a = 3,
-        b = 2;
+  const a = 3;
+  const b = 2;
 
   if (a + b < 4) {
     console.log('wrong');
@@ -14,16 +14,27 @@
   }
 }
 
+(a + b < 4) ? console.log('wrong') : console.log('too much');
+
 /** TODO
  * The system receives two params x and y.
  * Implement the logic when z equal to sum of x and y in case both of them less than 10 or multiplication in case of greater or equal than 10
  * Task 1 - if else
  * Task 2 - ternary operator
  */
-{
+
     const x = 3;
-    const y = 7;
-}
+    const y = 18;
+    let z;
+
+  if (x < 10 && y < 10) {
+     z = x + y;
+  } else { 
+     z = x * y; 
+    }
+
+z = (x < 10 && y < 10) ? x + y : x * y;
+console.log (z);
 
 /** TODO
  * The system receives 3 params - x, y, operator.
@@ -34,5 +45,22 @@
 {
     const x = 3;
     const y = 7;
-    const operator = "add";
+    let operator = 'multiply';
+
+
+switch (operator) {
+  case operator = 'subtract':     
+    console.log(x-y);     
+    break;
+  case operator = 'multiply':     
+    console.log(x*y);    
+    break;
+  case operator = 'divide':    
+    console.log(x/y);    
+    break;
+  default:    
+    console.log(x+y);    
+    break;
+}
+
 }
--- tasks/cycles/cycles.js
+++ tasks/cycles/cycles.js
@@ -13,7 +13,20 @@
 function calculateDiscount(redemption) {
   let discount;
 
-  //PLACE YOUR CODE HERE
+  switch (true) {
+    case redemption >= 0 && redemption <= 350:       
+      console.log('0%!');       
+      break;
+    case redemption >= 351 && redemption <= 1350:       
+      console.log('15%');      
+      break;
+    case redemption >= 1351 && redemption <= 2700:      
+      console.log('30%');      
+      break;
+    case redemption >= 2701 && redemption <= 6500:       
+      console.log('45%');       
+      break;
+  }
 
   return discount;
 }
@@ -23,6 +36,12 @@ function calculateDiscount(redemption) {
  */
 {
   const i = 10; //10! = 3628800
+  let y = 1;
+
+  for (k=1; k <= i; k++) {
+    y = k*y;
+    console.log ('factorial of ', k, ' = ', y);
+  }
 }
 
 /**
@@ -30,6 +49,12 @@ function calculateDiscount(redemption) {
  */
 {
   const substr = ["I", " love", " JS"];
+  let k = '';
+
+  for (let key of substr) {
+    k += key;  
+  }
+  console.log(k);
 }
 
 /**
@@ -42,6 +67,14 @@ function calculateDiscount(redemption) {
     interestOnDeposit: 250,
     otherExpences: -300
   };
-}
 
+  const array = Object.values(personIncomes);
+  let total = 0;
+
+  for (let income of array) {
+    total += income;    
+  }
+  console.log(total);
+
+}
 module.exports = calculateDiscount;
