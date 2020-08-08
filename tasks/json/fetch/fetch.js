7dd613985defab18450354939052981f1f83fd64
Sviatlana Pashko <s11.pashko@gmail.com>
2020-08-08 20:09:39 +0300

    task2

--- tasks/json/fetch/fetch.js
+++ tasks/json/fetch/fetch.js
@@ -5,3 +5,30 @@
  * DOCS: https://www.npmjs.com/package/node-fetch
  */
 const fetch = require('node-fetch');
+const fs = require('fs');
+const path = require('path');
+
+const filePath = path.join(__dirname, 'response.json');
+
+async function getRequest() {
+    let result = await fetch('https://jsonplaceholder.typicode.com/posts');
+    let data = await result.json();
+    let arr2 = data.filter(item => {
+        return item.id < 20;
+    });
+    fs.appendFile(filePath, JSON.stringify(arr2), err => {
+        if (err) {
+            throw err;
+        }
+    })
+};
+
+getRequest();
+
+
+
+
+
+// fetch('https://jsonplaceholder.typicode.com/posts')
+//     .then(response => response.json())
+//     .then(json => console.log(json));
